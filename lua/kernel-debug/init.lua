34cfacb7e3df703a9c83aca7b511fcb3118129d4
Martin Boros <mboros04@gmail.com>
2024-07-03 07:20:27 -0400

    try a different approach

--- lua/kernel-debug/init.lua
+++ lua/kernel-debug/init.lua
@@ -1,58 +1,42 @@
 local M = {}
+
 -- Function to setup the plugin
 function M.setup(opts)
 	-- Default options
 	opts = opts or {}
 	print("kernel-debug.nvim loaded with options: ", vim.inspect(opts))
+
 	-- Create the custom command
 	vim.api.nvim_create_user_command("KernelDebug", function()
 		M.show_popup()
 	end, { nargs = 0 })
 end
+
 -- Function to create and show the pop-up
 function M.show_popup()
-	local Popup = require("nui.popup")
-	local Layout = require("nui.layout")
-	local Input = require("nui.input")
-	local event = require("nui.utils.autocmd").event
+	local buf = vim.api.nvim_create_buf(false, true)
+	local width = vim.api.nvim_get_option("columns")
+	local height = vim.api.nvim_get_option("lines")
+	local win_opts = {
+		style = "minimal",
+		relative = "editor",
+		width = math.ceil(width * 0.8),
+		height = math.ceil(height * 0.8),
+		row = math.ceil(height * 0.1),
+		col = math.ceil(width * 0.1),
+	}
+	local win = vim.api.nvim_open_win(buf, true, win_opts)
 
-	M.top_left_popup = Popup({ border = "single" })
-	M.top_right_popup = Popup({ border = "single" })
-	M.bottom_input = Input({
-		border = "single",
-	}, {
-		prompt = "> ",
-		default_value = "",
-		on_submit = function(value)
-			print("Value submitted: ", value)
-			M.show_popup()
-		end,
-	})
-
-	M.layout = Layout(
-		{
-			position = "50%",
-			size = {
-				width = "90%",
-				height = "90%",
-			},
-		},
-		Layout.Box({
-			Layout.Box({
-				Layout.Box(M.top_left_popup, { size = "55%" }),
-				Layout.Box(M.top_right_popup, { size = "45%" }),
-			}, { dir = "row", size = "95%" }),
-			Layout.Box(M.bottom_input, { size = "5%" }),
-		}, { dir = "col" })
-	)
-
-	-- Keybinding to close the windows
-	local function close_windows()
-		M.top_right_popup:unmount()
-		M.top_left_popup:unmount()
-		M.bottom_input:unmount()
-		M.layout:unmount()
+	local function update_buffer_with_output(output)
+		vim.api.nvim_buf_set_lines(buf, -1, -1, false, output)
 	end
+
+	-- Function to close the floating window and buffer
+	local function close_windows()
+		vim.api.nvim_win_close(win, true)
+		vim.api.nvim_buf_delete(buf, { force = true })
+	end
+
 	-- Map <leader>kq to close the windows
 	vim.keymap.set(
 		"n",
@@ -60,6 +44,32 @@ function M.show_popup()
 		close_windows,
 		{ noremap = true, silent = true, desc = "Close Kernel Debug window" }
 	)
-	M.layout:mount()
+
+	-- Function to run the qemu command
+	local function run_qemu()
+		local cmd = "qemu-system-x86_64 -hda /path/to/image.qcow2" -- Replace with your qemu command
+
+		vim.fn.jobstart(cmd, {
+			stdout_buffered = true,
+			stderr_buffered = true,
+			on_stdout = function(_, data)
+				if data then
+					update_buffer_with_output(data)
+				end
+			end,
+			on_stderr = function(_, data)
+				if data then
+					update_buffer_with_output(data)
+				end
+			end,
+			on_exit = function(_, code)
+				update_buffer_with_output({ "Process exited with code " .. code })
+			end,
+		})
+	end
+
+	-- Run the qemu command
+	run_qemu()
 end
+
 return M
